3ede4fef4425ec9e752ff5bc1679566a8c46507a
nstellitano <nstellitano@yahoo.com>
2015-08-15 21:06:36 -0400

    continued to originize model and view

--- js/model/model.js
+++ js/model/model.js
@@ -1,7 +1,7 @@
 /**
  * Created by nicholasstellitano on 8/5/15.
  */
-var hood = hood || {};
+var neighborhood = neighborhood || {};
 'use strict';
 
 //Set destination for London.  Can be easily turned into a file that contains numerous vacation destinations
@@ -15,12 +15,14 @@ var destinations_london = [
         lat: 51.5008,
         long: -.1247,
         description: "Big Clock",
+        meta: ["tourist", "kids"],
         visible: true
     },
     {   name: "St Paul's Cathedral",
         lat: 51.5136,
         long: -.0981,
         description: "Very Old Church",
+        meta: ["tourist", "kids"],
         visible: true
     }
 ];
@@ -37,8 +39,48 @@ var hood = function(destinations) {
     }.bind(this);  // Ensure that "this" is always this view model
 };
 
-var neighborhood = new hood(destinations_london);
-ko.applyBindings(neighborhood);
-//console.log(test.dest()[0].name)
+//Define and build the map utilizing the origin and neighborhood based on the desired locaiton of the user
+var map = function(origin, neighborhood) {
+    this.initialize = function() {
+        var geocoder = new google.maps.Geocoder();
+        //directionsDisplay = new google.maps.DirectionsRenderer();
+
+        var mapOptions = {
+            zoom: 11,
+            center: origin
+        };
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        //directionsDisplay.setMap(map);
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker;
+
+        for (var i = 1; i < neighborhood.dest().length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(neighborhood.dest()[i].lat, neighborhood.dest()[i].long),
+                map: map
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+
+                return function () {
+
+                    //dest.A = marker.position.A;
+                    //dest.F = marker.position.F
+
+                    infowindow.setContent(neighborhood.dest()[i].name);
+                    infowindow.open(map, marker);
+
+                    map.setZoom(11);
+                    map.setCenter(marker.getPosition());
+                }
+            })(marker, i));
+        }
+    }
+};
+
+
 
 
---
+++ js/view_model/view_model.js
@@ -0,0 +1,22 @@
+/**
+ * Created by nicholasstellitano on 8/15/15.
+ */
+
+
+var octopus = {
+
+    init: function(){
+
+    },
+
+    setNeighborhood: function(){
+
+    },
+
+    buildMap: function(){
+
+
+    }
+
+
+}
